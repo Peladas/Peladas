715b76999886833aa62c0c1512390c79d7af4752
Marcelo-Freitas <101808921+Marcelo-Freitas@users.noreply.github.com>
2024-07-29 15:27:49 -0300

    models atualization

---
+++ app/Models/Users.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace App\Models;
+
+class Users {
+    public $tipo_usuario;
+    public $email;
+    public $telefone;
+    public $endereco;
+    public $senha;
+    public $situacao;
+}
